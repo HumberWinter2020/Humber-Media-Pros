418e07d11261f5a47bb77243f0bd8db89a9bd53b
Ivandi Santoso <ivandisantoso@Vahns-MacBook-Pro.local>
2020-02-26 02:08:44 -0500

    working on header

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -4,7 +4,16 @@ export default class Home extends Page {
 	constructor() {
 		super()
 		this.html = `
-			<h2>Home</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, excepturi placeat deserunt a sint tempore iure delectus. Explicabo tenetur eligendi rerum consequatur adipisci! Non recusandae provident placeat facere aut architecto!</p>`
+		<div class="program">
+		<h2>Program Name</h2>
+	</div>
+	<div class="main-section">
+	<h2>Home</h2>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, excepturi placeat deserunt a sint tempore iure delectus. Explicabo tenetur eligendi rerum consequatur adipisci! Non recusandae provident placeat facere aut architecto!</p>
+	</div>
+	<div class="course">
+		<h2>Courses</h2>
+	</div>
+			`
 	}
 }
