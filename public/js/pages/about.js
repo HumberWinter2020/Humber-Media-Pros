ee1c45aa34e9abf0814c4bb7bd3860ab6b58f9cb
Weixiang Lou <weixianglou@WeixiangdeMacBook-Pro.local>
2020-03-01 16:31:47 -0500

    about page update

--- public/js/pages/about.js
+++ public/js/pages/about.js
@@ -3,50 +3,69 @@ import Page from '../page.js'
 export default class About extends Page {
 	constructor() {
 		super()
-		this.html = `<h2>Programs</h2>
+		this.html = `<h2 class="about-title">Programs</h2>
 		<article class="cover">
-        <img src="../img/conor.jpg" />
+        <img src="../img/conor.jpg" class="about-title-image"/>
     </article>
-    <h2>Welcome to Humber Media Programs</h2>
+    <p class="about-media-title">Welcome to Humber Media Programs</p>
     <section class="programs_intro">
         <article class="program">
-            <img src="../img/conor.jpg" />
-            <h4>Web design and development</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum sit officiis perferendis molestias a possimus ipsum praesentium. Dolor aperiam laborum facere exercitationem doloremque consequuntur est deserunt incidunt maiores ex.</p>
-            <button><a href="#Wddm">Explore</a></button>
+            <img src="../img/web-image.png" class="about-program-image"/>
+            <h4 class="about-program-title">Web design and development</h4>
+            <ul class="about-benefit">
+            <li>Become a career-ready web designer in 1 year</li>
+            <li>Gain a comprehensive education from highly-talented and industry-experienced faculty</li>
+            <li>Use various industry-standard software in our state-of-the-art labs </li>
+            <li>Graduate from the program with a captivating portfolio website to present to potential employers</li>
+            <li>Make connections with industry professionals during networking opportunities</li>
+            </ul>
+            <br>
+            <br>
+            <br>
+            <br>
+            <a href="#Wddm"><button class="about-explore-btn">Explore</button></a>
             
         </article>
         <article class="program">
-            <img src="../img/conor.jpg" />
-            <h4>Graphic Design For Print and Web</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum sit officiis perferendis molestias a possimus ipsum praesentium. Dolor aperiam laborum facere exercitationem doloremque consequuntur est deserunt incidunt maiores ex.</p>
-            <button><a href="#grd">Explore</a></button>
+            <img src="../img/graphic-image.png" class="about-program-image"/>
+            <h4 class="about-program-title">Graphic Design For Print and Web</h4>
+            <ul class="about-benefit">
+            <li>Become a career-ready graphic designer in 1 year</li>
+            <li>Gain a comprehensive education from highly-talented and industry-experienced faculty</li>
+            <li>Use industry-standard software like Adobe Creative Cloud in our state-of-the-art labs</li>
+            <li>Graduate the program with a portfolio to present to potential employers</li>
+            <li>Make connections with industry professionals by participating in a portfolio show</li>
+            </ul>
+            <a href="#grd"><button class="about-explore-btn">Explore</button></a>
         </article>
         <article class="program">
-            <img src="../img/conor.jpg" />
-            <h4>3D Modelling and Visual Effects Production</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum sit officiis perferendis molestias a possimus ipsum praesentium. Dolor aperiam laborum facere exercitationem doloremque consequuntur est deserunt incidunt maiores ex.</p>
-            <button><a href="#dmv">Explore</a></button>
+            <img src="../img/3D-image.jpg" class="about-program-image"/>
+            <h4 class="about-program-title">3D Modelling and Visual Effects Production</h4>
+            <ul class="about-benefit">
+            <li>Flexibility for industry professionals looking to develop new skills</li>
+            <li>Approved for second career candidates</li>
+            <li>Course curriculum taught and designed by industry professionals</li>
+            <li>Teaching process and course structure follows a “pipelines process,” mirroring the realities of a real-world production environment</li>
+            </ul>
+            <a href="#dmv"><button class="about-explore-btn">Explore</button></a>
         </article>
     </section>
 	<section class="program_info" id="Wddm">
 		<h2>Web Design and Development</h2>
-        <p>You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
-        </p>
-        <p>
-            Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
-        </p>
-        <p>
-            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
-        </p>  
-        <p>
-            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+        <p>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
+
+        Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
+        
+        You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
+        
+        This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
         </p>
 	</section>
 	
 	<section class="program_details">
 	<article class="program_fee">
-		
+		<p> Domestic student: $9000 per two semesters</p>
+		<p> International student: $12000 per two semesters</p>
 	</article>
 	<article class="courses">
 	
@@ -55,16 +74,12 @@ export default class About extends Page {
 
 	<section class="program_info" id="grd">
 	<h2>Graphic Design for Print and Web</h2>
-        <p>You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
-        </p>
-        <p>
-            Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
-        </p>
-        <p>
-            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
-        </p>  
-        <p>
-            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+        <p>Motivated. Creative. Innovative. If these words describe you, then our intensive Graphic Design for Print and Web certificate program is for you. You will learn industry-best practices and how to communicate effective design solutions using composition, typography, illustration, interactive media and web design. Throughout our program, you will work towards developing a cutting-edge portfolio filled with real-world projects that will help you stand out in the creative field of graphic and web design.
+
+        In order to achieve the best, you will learn from the best. This program offers you access to the latest industry standards in hardware, design applications and instruction. Our labs are equipped with the latest Apple workstations, up-to-date Adobe Creative Cloud software and our course instruction is facilitated by industry-leading working professionals in their field of practice and study. Over the course of the program, you will build connections with industry professionals, alumni and community partners to help prepare you for future career opportunities, through supportive mentorship and coaching.
+        
+        This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding.  Further details and how to apply are found at ontario.ca/page/second-career.
+
         </p>
 	</section>
 	<section class="program_details">
@@ -81,21 +96,19 @@ export default class About extends Page {
 	
 	<section class="program_info" id="dmv">
 	<h2>3D Modelling and Visual Effects Production</h2>
-        <p>You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
-        </p>
-        <p>
-            Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
-        </p>
-        <p>
-            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
-        </p>  
-        <p>
-            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+        <p>Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life. You will develop skills in modelling, texturing and lighting, as well as in visual effects, compositing and digital cinematography. We employ a teaching process and course structure that requires content created in one course to be used to complete assignments in other courses. This “pipeline” process mirrors that of industry, allowing students to make a seamless transition into a real production environment.
+
+        The program is designed by artists for experienced traditional artists and animators who want to either develop or transition their skill sets for employment in the 3D and visual effects industries. The faculty take their years of industry experience into the classroom, straight from the studio. Their collective experience and passion in the gaming, television and film industries is the driving force that ensures the program meets the current and future needs of the industry.
+        
+        January intake scheduling: students will benefit from our streamlined flexible schedule, allowing them to work their day jobs while they study for their dream jobs. Our schedule will have students on campus and in classes on evenings and weekends. Students will study in our state-of-the-art labs equipped with Cintiq tablets and the latest tools and software.
+        
+        This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
         </p>
 	</section>
 	<section class="program_details">
 	<article class="program_fee">
-		
+		<p> Domestic student: $9000 per two semesters</p>
+		<p> International student: $12000 per two semesters</p>
 	</article>
 	<article class="courses">
 	
