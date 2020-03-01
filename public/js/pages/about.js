0333c6993cb559a06fbfa995ffd9df97b4cbbefe
sashidhar-meesala <sashidharklu@gmail.com>
2020-03-01 00:04:57 -0500

    added programs webpage

--- public/js/pages/about.js
+++ public/js/pages/about.js
@@ -3,6 +3,106 @@ import Page from '../page.js'
 export default class About extends Page {
 	constructor() {
 		super()
-		this.html = '<h2>About</h2>'
+		this.html = `<h2>Programs</h2>
+		<article class="cover">
+        <img src="../img/conor.jpg" />
+    </article>
+    <h2>Welcome to Humber Media Programs</h2>
+    <section class="programs_intro">
+        <article class="program">
+            <img src="../img/conor.jpg" />
+            <h4>Web design and development</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum sit officiis perferendis molestias a possimus ipsum praesentium. Dolor aperiam laborum facere exercitationem doloremque consequuntur est deserunt incidunt maiores ex.</p>
+            <button><a href="#Wddm">Explore</a></button>
+            
+        </article>
+        <article class="program">
+            <img src="../img/conor.jpg" />
+            <h4>Graphic Design For Print and Web</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum sit officiis perferendis molestias a possimus ipsum praesentium. Dolor aperiam laborum facere exercitationem doloremque consequuntur est deserunt incidunt maiores ex.</p>
+            <button><a href="#grd">Explore</a></button>
+        </article>
+        <article class="program">
+            <img src="../img/conor.jpg" />
+            <h4>3D Modelling and Visual Effects Production</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora earum sit officiis perferendis molestias a possimus ipsum praesentium. Dolor aperiam laborum facere exercitationem doloremque consequuntur est deserunt incidunt maiores ex.</p>
+            <button><a href="#dmv">Explore</a></button>
+        </article>
+    </section>
+	<section class="program_info" id="Wddm">
+		<h2>Web Design and Development</h2>
+        <p>You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
+        </p>
+        <p>
+            Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
+        </p>
+        <p>
+            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
+        </p>  
+        <p>
+            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+        </p>
+	</section>
+	
+	<section class="program_details">
+	<article class="program_fee">
+		
+	</article>
+	<article class="courses">
+	
+	</article>
+	</section>
+
+	<section class="program_info" id="grd">
+	<h2>Graphic Design for Print and Web</h2>
+        <p>You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
+        </p>
+        <p>
+            Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
+        </p>
+        <p>
+            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
+        </p>  
+        <p>
+            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+        </p>
+	</section>
+	<section class="program_details">
+	<article class="program_fee">
+		<p> Domestic student: $9000 per two semesters</p>
+		<p> International student: $12000 per two semesters</p>
+	</article>
+	<article class="courses">
+		
+	</article>
+	</section>
+
+	</section>
+	
+	<section class="program_info" id="dmv">
+	<h2>3D Modelling and Visual Effects Production</h2>
+        <p>You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
+        </p>
+        <p>
+            Our unique project-based approach leads students in the program through the entire process of creating websites and web applications. You will build static and dynamic websites utilizing the most current versions of industry-standard applications and tools. Programming and design skills are taught with a focus on following best practices and established industry standards including designing websites that are mobile-ready, accessible and future-proof.
+        </p>
+        <p>
+            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
+        </p>  
+        <p>
+            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.
+        </p>
+	</section>
+	<section class="program_details">
+	<article class="program_fee">
+		
+	</article>
+	<article class="courses">
+	
+	</article>
+	</section>
+
+    </section>
+	`
 	}
 }
