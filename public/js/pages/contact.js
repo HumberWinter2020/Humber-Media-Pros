ea2db6fe3d32afd922fa95a2f6c2146911369811
sashidhar-meesala <sashidharklu@gmail.com>
2020-03-01 00:07:20 -0500

    added profile page basic v1

--- public/js/pages/contact.js
+++ public/js/pages/contact.js
@@ -3,6 +3,52 @@ import Page from '../page.js'
 export default class Contact extends Page {
 	constructor() {
 		super()
-		this.html = '<h2>Contact</h2>'
+		this.html = `
+		<h2 style="text-align:center">Your Profile</h2>
+		<section class="main">
+    <article class="cover">
+        <img src="../img/conor.jpg" />
+    </article>
+    <article class="profile">
+        <img src="../img/conor.jpg" />
+    </article>
+</section>
+		<Section class="details_Section">
+		
+		<article>
+		<h3>Bio</h3>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, voluptatum molestias odit, vero perspiciatis vitae earum ut neque sint animi nobis corporis, amet modi fugit beatae illum! Reiciendis, dolor consectetur.
+		</article>
+		
+		<article>
+		<h3>Details</h3>
+          <h2>
+          First Name Last Name
+          </h2>
+          <p>
+          Graduated in year
+          </p>
+          <p>
+          Program: Program Name
+		  </p>
+		  <p>Skillset: HTML , CSS, JS</p>
+        </article>
+		</Section>
+		<h3>Connect with me on </h3>
+        <section class="connect">
+        <article class="connect_gridItem">
+          <i class="fab fa-facebook fa-7x"></i>
+        </article>
+        <article class="connect_gridItem">
+          <i class="fab fa-github fa-7x"></i>
+        </article>
+        <article class="connect_gridItem">
+          <i class="fab fa-linkedin fa-7x"></i>
+        </article>
+        <article class="connect_gridItem">
+          <i class="fab fa-codepen fa-7x"></i>
+        </article>
+        </section>`
 	}
 }
+
