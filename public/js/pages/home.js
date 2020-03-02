e2347baf3d60663f0eed55fc4b686adf655ffa23
Ivandi Santoso <n01398965@nj-00118-007.humber.org>
2020-03-02 12:42:38 -0500

    added content on the homepage

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -25,21 +25,13 @@ export default class Home extends Page {
 	<article class="people">
 		<section>
 			<h1>Alumni</h1>
-<<<<<<< HEAD
 			<p>Advancement and Alumni is committed to establishing lifelong connections with our 250,000+ graduates and strengthening our relationship with the Humber community. We want to stay connected with you, share news and stories about your career achievements, celebrate your successes, and provide you with value-added services and benefits.</p>
-=======
-			<p>View past humber media pro graduates, See thier skilset and projects </p>
->>>>>>> 63f40ae9b221ca342c62d6919d9be6c31783808c
 			<input type="button" value="View" class='view-btn'>
 		</section>
 
 		<section>
 			<h1>Staff</h1>
-<<<<<<< HEAD
 			<p>MyHumber provides staff and faculty with access to Academic Progress, grade entry, program rosters, Student Action Form (SAF) functionality, and additional information about courses, programs, and students.</p>
-=======
-			<p>Humber professors teaching at the Media building, talk to coordinators and staff</p>
->>>>>>> 63f40ae9b221ca342c62d6919d9be6c31783808c
 			<input type="button" value="View" class='view-btn'>
 		</section>
 	</article>
