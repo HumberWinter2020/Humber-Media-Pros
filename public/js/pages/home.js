9649f57597ebc3ce88c44182cefbe867fe11c5e9
QuentincJohnson <quentincjohnson@gmail.com>
2020-03-01 23:12:59 -0500

    finished home page exept for adding video

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -4,16 +4,39 @@ export default class Home extends Page {
 	constructor() {
 		super()
 		this.html = `
-		<div class="program">
-		<h2>Program Name</h2>
+		<div class="home-name">
+		<h2>Humber Media Pros</h2>
 	</div>
 	<div class="main-section">
-	<h2>Home</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, excepturi placeat deserunt a sint tempore iure delectus. Explicabo tenetur eligendi rerum consequatur adipisci! Non recusandae provident placeat facere aut architecto!</p>
+		<h2>Welcome!</h2>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, excepturi placeat deserunt a sint tempore iure delectus. Explicabo tenetur eligendi rerum consequatur adipisci! Non recusandae provident placeat facere aut architecto!</p>
 	</div>
-	<div class="course">
-		<h2>Courses</h2>
+
+	<h2 class="title">Courses</h2>
+	<div class="choices">
+		<section class="web">Web design and development</section>
+		<section class="print">Graphic Design For Print and Web</section>
+		<section class="mod">3D Modelling and Visual Effects Production</section>
 	</div>
+
+	<h2 class="title">People</h2>
+	<article class="people">
+		<section>
+			<h1>Alumni</h1>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. bitis, tempore enim dolor aut cum architecto est in!</p>
+			<input type="button" value="View" class='view-btn'>
+		</section>
+
+		<section>
+			<h1>Staff</h1>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. bitis, tempore enim dolor aut cum architecto est in!</p>
+			<input type="button" value="View" class='view-btn'>
+		</section>
+	</article>
+
+	<h2 class="title">Who We Are</h2>
+
+
 			`
 	}
 }
