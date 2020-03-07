a13ece03e6629ef70348662c9badc4c771bfa04b
Weixiang Lou <weixianglou@WeixiangdeMacBook-Pro.local>
2020-03-07 17:05:43 -0500

    profile update

--- public/js/pages/students.js
+++ public/js/pages/students.js
@@ -15,7 +15,7 @@ export default class Contact extends Page {
   
     <article class="student-info">    
       <fieldset class="about-student">
-        <legend for="about-student"><img src="img/icons/icons8-user-90.png" alt="">About</legend>
+        <legend for="about-student" class="profile-title">About</legend>
         <ul>
           <li>Student ID : </li>
           <li>First Name : </li>
@@ -23,13 +23,15 @@ export default class Contact extends Page {
           <li>Phone Number : </li>
           <li>Email : </li>
           <li>Address :</li>
+          </ul>
+          <ul class="profile-media">
           <li><a href="#"><img src="/img/icons/facebook.png" alt=""></a></li>
           <li><a href="#"><img src="/img/icons/linkedin.png" alt="" srcset=""></a></li>
           <li><a href="#"><img src="/img/icons/github.png" alt="" srcset=""></a></li>
         </ul>
       </fieldset>
       <fieldset class="student-skill">
-        <legend for="student-skill"><a href="/skills" alt="Skills">&nbsp; Skills</a> &nbsp;|&nbsp; <a href="/projects">Projects &nbsp;</a></legend>
+        <legend for="student-skill"><a href="/skills" alt="Skills" class="profile-title">&nbsp; Skills</a> &nbsp;|&nbsp; <a href="/projects" class="profile-title">Projects &nbsp;</a></legend>
         <ul>
           <li>1.</li>
           <li>2.</li>
